39ae51942e03bd6d56a8a204468f9ea3f69df2d6
Jack Krolik <belljack17@gmail.com>
2023-02-10 09:46:49 -0500

    adding for js changes

--- main.js
+++ main.js
@@ -3,19 +3,46 @@
 
 
 
-function init() {
-    let pt_list = document.getElementsByClassName("circle")
-    for (i=0; i < pt_list.length; i++) {
-        
-        pt_list[i].addEventListener("click", PtHover(pt_list, i));
-    }
-}
 
-
-
-init()
-
-function PtHover(pt_list, i) {
+function PtClick() {
 	
-	pt_list[i].toggle("red");
+
+	this.classList.toggle("red");
 }
+
+
+// get list of initial points
+let pt_list = document.getElementsByTagName("circle");
+
+// add event listener to each intial points
+for (i=0; i < pt_list.length; i++) {
+    
+    let pt = pt_list[i];
+    
+    pt.addEventListener("click", PtClick);
+}
+
+
+
+function PtAdd() {
+    
+    let x_cord = (document.getElementById("x-cord").value) * 25 + 100;
+    let y_cord = 400 - ((document.getElementById("y-cord").value) * 25);
+
+
+    let svg = document.getElementById('chart');
+
+    circle = document.createElement('circle');
+    
+    circle.addEventListener("click", PtClick);
+
+    circle.setAttribute('cx', x_cord);
+    circle.setAttribute('cy', y_cord);
+    circle.setAttribute('r', 8);
+
+    svg.append(circle);
+}
+
+document.getElementById("addPt").addEventListener("click", PtAdd);
+
+
