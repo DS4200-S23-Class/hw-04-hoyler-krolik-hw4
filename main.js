b4eaab20a96eacd54fafd1e052469b58335ada6b
rhoyler7 <roberthoyler@gmail.com>
2023-02-12 15:24:27 -0500

    Comments Part 1

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-
-
-
-
-
-
+// function for clicking point, create red outline on clicked points
 function PtClick() {
-	
-
 	this.classList.toggle("red");
 }
 
@@ -16,22 +9,18 @@ let pt_list = document.getElementsByTagName("circle");
 
 // add event listener to each intial points
 for (i=0; i < pt_list.length; i++) {
-    
     let pt = pt_list[i];
-    
     pt.addEventListener("click", PtClick);
 }
 
 
-
+// function that adds point to the scatter plot
 function PtAdd() {
-    
     let x_cord = (document.getElementById("x-cord").value) * 50 + 100;
     let y_cord = 800 - ((document.getElementById("y-cord").value) * 50);
 
 
     let svg = document.getElementById('chart');
-
     let circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
     
     circle.addEventListener("click", PtClick);
